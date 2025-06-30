ea2cd5ffc0a1d530fa6effac4c51ff543f1b6fa1
rob587 <robertocamm02@yahoo.com>
2025-06-30 15:28:47 +0200

    first string

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -2,6 +2,10 @@
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
 
+const nome = "Roberto"
+
+console.log(nome)
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
